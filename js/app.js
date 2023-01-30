509d4a94aca3e1169ba1f0cd79432d9c0ab2d478
Cody Lobdell <thecreator85@gmail.com>
2023-01-30 07:47:30 -0800

    edited code block for seattle store. WIP

--- js/app.js
+++ js/app.js
@@ -1,49 +1,66 @@
 'use strict';
 
-let cities = document.getElementById('cities');
+let hours = ['6AM: ', '7AM: ', '8AM: ', '9AM: ', '10AM ', '11AM: ', '12PM: ', '1PM: ', '2PM: ', '3PM: ', '4PM: ', '5PM: ', '6PM: ', '7PM: '];
 
-let hour = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', ];
+//-create store object.
+//-create separate JS object literal. 
 
-const seattleStore = {
+let seattleStore = {
   location: 'Seattle',
+  min: 23,
   maxCust: 65,
-  minCust: 23,
-  avgCookiesPerCust: 6.3,
-  cookiesPerHour: [],
-
-  getRandomCustomersPerHour: function () {
-    return Math.floor((Math.random()*(this.maxCust - this.minCust + 1)+this.minCust));
+  avg: 6.3,
+  randomCust: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  randomSale: function () {
+    return Math.round((this.avg * this.randomCust()));
   },
 
-  generateCookiesPerHour: function () {
-    for (let i = 0; i < hour.length; i++) {
-      let customers = this.getRandomCustomersPerHour();
-      let cookies = Math.ceil(customers * this.avgCookiesPerCust);
-      this.cookiesPerHour.push(`${hour[i]} ${cookies} cookies.`);
+  //-for each hour at each location
+  //-using average cookies purchased
+  //-and random number of customers generated.
+
+  salesByHour: [],
+  salesNumber: function () {
+    for (let i = 0; i < hours.length; i++) {
+      this.salesByHourAr.push(
+        this.randomSale());
     }
   },
-
-  cookieCity: function(){
-    let citySection = document.createElement('section');
-    cities.appendChild(citySection);
-
-    let cityHead = document.createElement('h3');
-    cityHead.textContent = `${this.location}`;
-    citySection.appendChild(cityHead);
-
-    let cPH = document.createElement('ul');
-    citySection.appendChild(cPH);
-
-    for (let i =0; i < this.cookiesPerHour.length; i++){
-      let li = document.createElement('li');
-      li.textContent = `${this.cookiesPerHour[i]}`;
-      cPH.appendChild(li);
+  totalSale: function () {
+    let total = 0;
+    for (let k = 0; k < this.salesByHour.length; k++) {
+      // console.log(this.salesByHourAr[k]);
+      total += this.salesByHourAr[k];
     }
-
+    console.log(total);
+    return total;
   }
-};
 
-seattleStore.generateCookiesPerHour();
-console.log(seattleStore.cookiesPerHour);
-console.log(seattleStore.getRandomCustomersPerHour());
-seattleStore.cookieCity();
+};
+console.log(seattleStore.salesNumber());
+console.log(seattleStore.salesByHourAr);
+console.log(seattleStore.randomCust());
+console.log(` ${seattleStore.randomSale()} is randomSale`);
+console.log(`total sales ${seattleStore.totalSale()}`);
+
+let newDiv = document.getElementById('div'); //located the div on html, so Store name can be listed under
+// let newBody = document.body;
+// console.log(newBody);
+let seattleP = document.createElement('p'); //created h2 for store name
+seattleP.textContent = seattleStore.location; //pull the store name from obj
+newDiv.appendChild(seattleP); //add store nme under div
+
+// let newLi = document.getElementById('li');
+
+// print list
+for (let j = 0; j < hours.length; j++) {
+  let seattleLi = document.createElement('li');
+  seattleLi.textContent = `${hours[j]}: ${seattleStore.salesByHourAr[j]} cookies`;
+  seattleP.appendChild(seattleLi);
+}
+
+let seattleLiTotal = document.createElement('li');
+seattleLiTotal.textContent = `Total: ${seattleStore.totalSale()} cookies`;
+seattleP.appendChild(seattleLiTotal);
